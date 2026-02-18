7dbff267cd9e61d4f163b3eb04ade1e826265630
Khamzat-junior <dertunovdert@gmail.com>
2026-02-18 20:39:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//* две правильные переменные *//
+
+let name = "john";
+let age = 30;
+
+console.log(name, age);
+
+// * две неправильные переменные *//
+
+let 1Name = "Jonh";
+let my-name = "John";
+
+console.log(1Name, my-name);
